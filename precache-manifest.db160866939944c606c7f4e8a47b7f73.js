a39ab2bd8f76eceac6414b92c0362f55cd5d89bb
Danny Beltran <dannyb@uci.edu>
2019-05-06 14:13:02 -0700

    Updates

---
+++ precache-manifest.db160866939944c606c7f4e8a47b7f73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc61f9b25b2cc51cc196ead0c1efb4e4",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "42c0d58a651a45acdfb2",
+    "url": "/TodoApp/static/css/main.e2c0adeb.chunk.css"
+  },
+  {
+    "revision": "94c3165186f215582bb2",
+    "url": "/TodoApp/static/js/2.c37d2ede.chunk.js"
+  },
+  {
+    "revision": "42c0d58a651a45acdfb2",
+    "url": "/TodoApp/static/js/main.e85c85d7.chunk.js"
+  },
+  {
+    "revision": "b429d889341aa25a2548",
+    "url": "/TodoApp/static/js/runtime~main.5a342d52.js"
+  }
+]);
